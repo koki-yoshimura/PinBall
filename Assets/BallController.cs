3076869472e764b779fd8568357a76bc13b262c4
koki-yoshimura <fanta1536arm@gmail.com>
2019-04-21 18:26:41 +0900

    Pinboll
    作成しましたが全然分からないです。

---
+++ Assets/BallController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class BallController : MonoBehaviour
+{
+
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    GameObject director;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+ 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "LargeStarTag")
+        {
+            this.director.GetComponent<GameDirector>().largestar();
+        }
+        else if (other.gameObject.tag == "SmallStarTag")
+        {
+            this.director.GetComponent<GameDirector>().smallstar();
+        }
+        else if (other.gameObject.tag == "SmallCloudTag")
+        {
+            this.director.GetComponent<GameDirector>().smallcloud();
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            this.director.GetComponent<GameDirector>().largecloud();
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        this.director = GameObject.Find("GameDirector");
+
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+
+        }
+    }
+}
---
+++ Assets/GameDirector.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameDirector : MonoBehaviour {
+
+    private GameObject pointText;
+
+    int point = 0;
+
+    public void largestar()
+    {
+        this.point += 20;
+    }
+
+    public void smallstar()
+    {
+        this.point += 10;
+    }
+
+    public void largecloud()
+    {
+        this.point += 40;
+    }
+    public void smallcloud()
+    {
+        this.point += 30;
+    }
+
+    // Use this for initialization
+    void Start () {
+        this.pointText = GameObject.Find("point");
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        this.pointText.GetComponent<Text>().text = this.point.ToString() + " point";
+    }
+}
